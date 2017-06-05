f80c372150f8916a45d19a6ae60b789978146c66
Zhijia <z.jia.hu@gmail.com>
2017-06-06 07:58:02 +0800

    added stacks balanced brackets

---
+++ data_structures/stacks_balanced_brackets.py
@@ -0,0 +1,11 @@
+
+def is_matched(expression):
+    return True
+
+t = int(input().strip())
+for a0 in range(t):
+    expression = input().strip()
+    if is_matched(expression) == True:
+        print("YES")
+    else:
+        print("NO")
