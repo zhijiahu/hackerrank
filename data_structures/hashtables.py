3442604d0c4910d6bf285ec9fcbdcd7dd56fd41a
Zhijia <z.jia.hu@gmail.com>
2017-06-05 23:01:58 +0800

    hashtable

---
+++ data_structures/hashtables.py
@@ -0,0 +1,12 @@
+class HashTable:
+    def __init__():
+        self.array = []
+
+    def __setitem__(self, key, value):
+        pass
+
+    def _get_hash_value(key):
+        pass
+
+    def _get_index_from_hash(hash):
+        pass
