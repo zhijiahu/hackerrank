c4df90e3149dcc93367cfdd0fd27ce40e7245749
Zhijia <zhijia.hu@ubisoft.com>
2017-06-06 18:29:37 +0800

    half working stack_balanced_brackets, didn't pass all tests.

--- data_structures/stacks_balanced_brackets.py
+++ data_structures/stacks_balanced_brackets.py
@@ -1,5 +1,23 @@
 
+
 def is_matched(expression):
+
+    stack = []
+    for bracket in expression:
+        # Found a close bracket
+        if bracket == '}' or bracket == ']' or bracket == ')':
+            if not stack:
+                return False
+
+            last_open_bracket = stack.pop()
+
+            if (ord(last_open_bracket) + 1 != ord(bracket)) and \
+               (ord(last_open_bracket) + 2 != ord(bracket)):
+                return False
+        else:
+            # Assume opened brackets, push into stack
+            stack.append(bracket)
+
     return True
 
 t = int(input().strip())
