e3d2f5375f6bfbde619a74773b6c346f57687166
Zhijia <zhijia.hu@ubisoft.com>
2017-06-12 12:27:34 +0800

    added skeleton for bst

---
+++ data_structures/binary_search_trees.py
@@ -0,0 +1,16 @@
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+    
+    def insert(self, value):
+        pass
+
+    def contains(self, value):
+        pass
+
+    def print_in_order(self):
+        pass
+    
