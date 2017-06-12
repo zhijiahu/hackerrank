139a905411e77d9b719595b9dd42736e003dc5d3
Zhijia <zhijia.hu@ubisoft.com>
2017-06-12 17:00:27 +0800

    added a naive binary search tree

--- data_structures/binary_search_trees.py
+++ data_structures/binary_search_trees.py
@@ -6,11 +6,51 @@ class Node:
         self.right = None
     
     def insert(self, value):
-        pass
+        """
+        Naive way
+        """
+        if value < self.value:
+            if self.left is None:
+                self.left = Node(value)
+            else:
+                self.left.insert(value)
+        elif value > self.value:
+            if self.right is None:
+                self.right = Node(value)
+            else:
+                self.right.insert(value)
 
     def contains(self, value):
-        pass
+        if value < self.value:
+            if self.left is not None:
+                return self.left.contains(value)
+            else:
+                return False
+                
+        elif value > self.value:
+            if self.right is not None:
+                return self.right.contains(value)
+            else:
+                return False
+        else:
+            return True
 
     def print_in_order(self):
-        pass
-    
+        if self.left is not None:
+            self.left.print_in_order()
+
+        print('[{value}]'.format(value=self.value))
+
+        if self.right is not None:
+            self.right.print_in_order()
+
+
+if __name__ == "__main__":
+    n = Node(5)
+    n.insert(3)
+    n.insert(7)
+    n.insert(1)
+    n.insert(15)
+    n.insert(6)
+
+    n.print_in_order()
