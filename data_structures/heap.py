8ce85e3177e05b7e2c09d0b34ed8df457b3cbced
Zhijia <zhijia.hu@ubisoft.com>
2017-06-21 17:29:27 +0800

    added skeleton for heap

---
+++ data_structures/heap.py
@@ -0,0 +1,18 @@
+
+class Heap:
+    def __init__(self):
+        self.data = []
+
+    def __get_parent_index(index):
+        return (index - 1) // 2
+
+    def __get_left_index(parent_index):
+        return (parent_index * 2) + 1
+
+    def __get_right_index(parent_index):
+        return (parent_index * 2) + 2
+
+
+    def insert(value):
+        pass
+
