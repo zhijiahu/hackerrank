96c3dc91a4fb3f9557ea9c4ee5a9b7cda7381f88
Zhijia <zhijia.hu@ubisoft.com>
2017-06-27 18:29:06 +0800

    almost done with heap. left `sift_down`

--- data_structures/heap.py
+++ data_structures/heap.py
@@ -9,7 +9,7 @@ class Heap:
 
     """
     def __init__(self):
-        self.data = []
+        self.__items = []
 
     def __get_parent_index(index):
         return (index - 1) // 2
@@ -20,19 +20,53 @@ class Heap:
     def __get_right_index(parent_index):
         return (parent_index * 2) + 2
 
-    def peek(self):
-        if self.data:
-            return self.data[0]
+    def __has_left_child(index):
+        return __get_left_index(index) < len(self.__items)
 
-    def push(self, value):
-        self.data.append(value)
-        self.sift_up()
+    def __has_right_child(index):
+        return __get_right_index(index) < len(self.__items)
+
+    def __has_parent(index):
+        return __get_parent_index(index) >= 0
+
+    def __left_child(index):
+        return self.__items[self.__get_left_index(index)]
+
+    def __right_child(index):
+        return self.__items[self.__get_right_index(index)]
+
+    def __parent(index):
+        return self.__items[self.__get_parent_index(index)]
+
+    def __swap(index_one, index_two):
+        temp = self.__items[index_one]
+        self.__items[index_one] = self.__items[index_two]
+        self.__items[index_two] = temp
+
+    def peek(self):
+        return self.__items[0]
 
     def poll(self):
-        first = self.data[0]
-        last = self.data.pop()
-        self.data[0] = last
+        first = self.__items[0]
+        last = self.__items.pop()
+        self.__items[0] = last
 
-        self.sift_down()
+        self.__sift_down()
 
         return first
+
+    def add(self, value):
+        self.__items.append(value)
+        self.__sift_up()
+
+    def __sift_up():
+        # start with last index
+        index = len(self.__items) - 1
+
+        while self.__has_parent(index) and (self.__parent(index) > self._items[index]):
+            parent_index = self.__get_parent_index(index)
+            self.__swap(index, parent_index)
+            index = parent_index
+
+    def __sift_down():
+        pass
