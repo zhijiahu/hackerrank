1890fbff4c85502bef13a6cc8cc16bbed067ca97
Zhijia <zhijia.hu@ubisoft.com>
2017-06-29 18:38:19 +0800

    completed heap_running_medium....might have bugs

--- data_structures/heap.py
+++ data_structures/heap.py
@@ -11,39 +11,42 @@ class Heap:
     def __init__(self):
         self.__items = []
 
-    def __get_parent_index(index):
+    def __get_parent_index(self, index):
         return (index - 1) // 2
 
-    def __get_left_index(parent_index):
+    def __get_left_index(self, parent_index):
         return (parent_index * 2) + 1
 
-    def __get_right_index(parent_index):
+    def __get_right_index(self,parent_index):
         return (parent_index * 2) + 2
 
-    def __has_left_child(index):
-        return __get_left_index(index) < len(self.__items)
+    def __has_left_child(self, index):
+        return self.__get_left_index(index) < len(self.__items)
 
-    def __has_right_child(index):
-        return __get_right_index(index) < len(self.__items)
+    def __has_right_child(self, index):
+        return self.__get_right_index(index) < len(self.__items)
 
-    def __has_parent(index):
-        return __get_parent_index(index) >= 0
+    def __has_parent(self, index):
+        return self.__get_parent_index(index) >= 0
 
-    def __left_child(index):
+    def __left_child(self, index):
         return self.__items[self.__get_left_index(index)]
 
-    def __right_child(index):
+    def __right_child(self, index):
         return self.__items[self.__get_right_index(index)]
 
-    def __parent(index):
+    def __parent(self, index):
         return self.__items[self.__get_parent_index(index)]
 
-    def __swap(index_one, index_two):
+    def __swap(self, index_one, index_two):
         temp = self.__items[index_one]
         self.__items[index_one] = self.__items[index_two]
         self.__items[index_two] = temp
 
     def peek(self):
+        if not self.__items:
+            return None
+
         return self.__items[0]
 
     def poll(self):
@@ -59,14 +62,30 @@ class Heap:
         self.__items.append(value)
         self.__sift_up()
 
-    def __sift_up():
+    def __sift_up(self):
         # start with last index
         index = len(self.__items) - 1
 
-        while self.__has_parent(index) and (self.__parent(index) > self._items[index]):
+        while self.__has_parent(index) and (self.__parent(index) > self.__items[index]):
             parent_index = self.__get_parent_index(index)
             self.__swap(index, parent_index)
             index = parent_index
 
-    def __sift_down():
-        pass
+    def __sift_down(self):
+        # start with first index
+        index = 0
+
+        while self.__has_left_child(index):
+            # find which child is smaller, in order to compare with current item
+            # check left first
+            smaller_child_index = self.__get_left_index(index)
+            if self.__has_right_child(index) and self.__right_child(index) < self.__left_child(index):
+                smaller_child_index = self.__get_right_index(index)
+
+            # if child is smaller, swap!
+            if self.__items[smaller_child_index] < self.__items[index]:
+                self.__swap(index, smaller_child_index)
+                index = smaller_child_index
+            else:
+                break
+
--- data_structures/heap_running_median.py
+++ data_structures/heap_running_median.py
@@ -1,10 +1,37 @@
 
 import sys
 
+from heap import Heap
+
 
 n = int(input().strip())
 a = []
 a_i = 0
+
+heap = Heap()
+
 for a_i in range(n):
    a_t = int(input().strip())
    a.append(a_t)
+   heap.add(a_t)
+
+   # number of pops to get to medium indices
+   if a_i % 2 == 0:
+      medium_index = (a_i + 1) // 2
+   else:
+      medium_index = (a_i + 1) // 2 - 1
+
+   polled = []
+   for i in range(medium_index):
+      polled.append(heap.poll())
+
+   if a_i % 2 == 0:
+      print(heap.peek() / 1)
+   else:
+      item = heap.poll()
+      polled.append(item)
+      print((heap.peek() + item) / 2)
+
+   # add those polled back
+   for item in polled:
+      heap.add(item)
