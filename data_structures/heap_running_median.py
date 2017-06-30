be1fb1a79c63d319933f5b266019c6cc21573a4d
Zhijia <z.jia.hu@gmail.com>
2017-06-30 08:02:27 +0800

    heap running medium is too slow....need 2 heaps?

--- data_structures/heap_running_median.py
+++ data_structures/heap_running_median.py
@@ -1,19 +1,15 @@
 
 import sys
+import random
+import copy
 
 from heap import Heap
 
+min_heap = Heap()
 
-n = int(input().strip())
-a = []
-a_i = 0
-
-heap = Heap()
-
-for a_i in range(n):
-   a_t = int(input().strip())
-   a.append(a_t)
-   heap.add(a_t)
+def get_running_medium(a_i, a_t):
+   global min_heap
+   min_heap.add(a_t)
 
    # number of pops to get to medium indices
    if a_i % 2 == 0:
@@ -23,15 +19,30 @@ for a_i in range(n):
 
    polled = []
    for i in range(medium_index):
-      polled.append(heap.poll())
+      polled.append(min_heap.poll())
 
    if a_i % 2 == 0:
-      print(heap.peek() / 1)
+      print(min_heap.peek() / 1)
    else:
-      item = heap.poll()
+      item = min_heap.poll()
       polled.append(item)
-      print((heap.peek() + item) / 2)
+      print((min_heap.peek() + item) / 2)
 
    # add those polled back
    for item in polled:
-      heap.add(item)
+      min_heap.add(item)
+
+# n = int(input().strip())
+# a = []
+# a_i = 0
+
+# for a_i in range(n):
+#    a_t = int(input().strip())
+#    get_running_medium(a_i, a_t)
+
+
+if __name__ == "__main__":
+   random_list = random.sample(range(1000), 1000)
+
+   for index, item in enumerate(random_list):
+      get_running_medium(index, item)
