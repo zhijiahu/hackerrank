f87ceafd7b4485538c40b458d18795bb84d78e47
Zhijia <z.jia.hu@gmail.com>
2017-07-03 22:24:41 +0800

    tries_contact skeleton

---
+++ data_structures/tries.py
@@ -0,0 +1,3 @@
+
+class Tries:
+    pass
---
+++ data_structures/tries_contact.py
@@ -0,0 +1,16 @@
+
+
+
+def add_name(name):
+    pass
+
+def find_partial(partial):
+    pass
+
+def run_with_user_input():
+    n = int(input().strip())
+    for a0 in range(n):
+        op, contact = input().strip().split(' ')
+
+if __name__ == '__main__':
+    run_with_user_input()
