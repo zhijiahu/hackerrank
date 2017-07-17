65b31fcb619b28591db19d3a7baf67ff33d36f53
Zhijia <z.jia.hu@gmail.com>
2017-07-17 09:09:39 +0800

    incomplete tries...

--- data_structures/tries.py
+++ data_structures/tries.py
@@ -1,3 +1,13 @@
 
 class Tries:
     pass
+
+
+class Node:
+    def __init__(self, data):
+        self.__data = data
+        self.__children = {}
+        self._is_complete = False
+
+    def add_child(self, data):
+        pass
