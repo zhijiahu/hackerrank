e1372131b5e409dc03d2d03fdb509ef7d703482e
Zhijia <z.jia.hu@gmail.com>
2017-07-19 22:33:15 +0800

    add child to tries

--- data_structures/tries.py
+++ data_structures/tries.py
@@ -1,13 +1,39 @@
 
 class Tries:
-    pass
+    """
+        c
+       / \
+      a   i
+     / \   \
+    t   r   d
+
+    """
+
+    def __init__(self):
+        self.tries = Node()
+
+    def add(self, word):
+        self.tries.add_child(word)
+
+    def start_with_prefix(self, prefix):
+        pass
+
+    def has_word(self, word):
+        pass
 
 
 class Node:
-    def __init__(self, data):
-        self.__data = data
-        self.__children = {}
+    def __init__(self):
         self._is_complete = False
+        self.__children = {}
 
-    def add_child(self, data):
-        pass
+    def add_child(self, word):
+        first_letter = word[0]
+
+        if len(word) > 1:
+            if first_letter not in self.__children:
+                node = Node()
+                node.add_child(word[1:])
+                self.__children[first_letter] = node
+            else:
+                self.__children[first_letter].add_child(word[1:])
--- data_structures/tries_contact.py
+++ data_structures/tries_contact.py
@@ -1,8 +1,9 @@
+from tries import Tries
 
-
+tries = Tries()
 
 def add_name(name):
-    pass
+    tries.add(name)
 
 def find_partial(partial):
     pass
@@ -11,6 +12,10 @@ def run_with_user_input():
     n = int(input().strip())
     for a0 in range(n):
         op, contact = input().strip().split(' ')
+        if op == 'add':
+            add_name(contact)
+        elif op == 'find':
+            find_partial(contact)
 
 if __name__ == '__main__':
     run_with_user_input()
