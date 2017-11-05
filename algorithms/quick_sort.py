2c8d903b3731ba01557668c654e77d3090cec6ac
Zhijia <z.jia.hu@gmail.com>
2017-11-05 12:45:55 +0800

    completed challenge but not yet using my own quick sort

--- algorithms/quick_sort.py
+++ algorithms/quick_sort.py
@@ -1,5 +1,23 @@
 
 from functools import cmp_to_key
+from itertools import zip_longest
+
+
+class QuickSort():
+
+    def sort(array):
+        """
+        1. Choose a pivot from the current segment with right and left pointers
+           at last and first element
+        2. Move left pointer until it is pointing at an element > pivot
+        3. Move right pointer until it is pointing at an element < pivot
+        4. Swap the 2 elements
+        5. Partition from first to 1 element before RIGHT and from RIGHT to last
+        6. Perform the same logic on each partitions
+        """
+        pass
+
+
 class Player:
     def __init__(self, name, score):
         self.name = name
@@ -10,12 +28,23 @@ class Player:
 
     def comparator(a, b):
         if a.score > b.score:
-            return 1
-        elif b.score > a.score:
             return -1
+        elif b.score > a.score:
+            return 1
 
-        # scores are equal, compare alphabets
+        char_pairs = zip_longest(a.name, b.name)
+        for pair in char_pairs:
+            if pair[0] is None:
+                return -1
+            if pair[1] is None:
+                return 1
 
+            if pair[0] > pair[1]:
+                return 1
+            elif pair[1] > pair[0]:
+                return -1
+
+        return 0 # same
 
 n = int(input())
 data = []
